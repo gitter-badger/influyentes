c6a4f2366306066ff7414998b0b0249f6ec7b256
Mohamed Ziata <wakematta@gmail.com>
2016-05-14 14:38:02 +0200

    Remove some obsolet code

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -54,48 +54,4 @@ class PostPolicy < ApplicationPolicy
   def record_of_user?
     record.user == user
   end
-
-  # def record_published?
-  #   record.published?
-  # end
-  #
-  #
-  # # Check if user can access to administration panel
-  # def allowed_to_view?
-  #   user && ( user.administrator? || user.moderator? || user.reviewer? )
-  # end
-  #
-  # def allowed_to_edit?
-  #   user && ( user.administrator? || user.moderator? )
-  # end
-  #
-  # def allowed_to_publish?
-  #   allowed_to_view? || record_of_user?
-  # end
-  #
-  # def allowed_to_unpublish?
-  #   allowed_to_edit? || record_of_user?
-  # end
-
-
-  # def can?(action)
-  #   return false if banned? || deleted?
-  #
-  #   case action
-  #   when "review"
-  #     ( reviewer? || moderator? || administrator? )
-  #   when "publish"
-  #     ( reviewer? || moderator? || administrator? )
-  #   when "unpublish"
-  #     ( moderator? || administrator? )
-  #   when "deleted"
-  #     ( administrator? )
-  #   else
-  #     false
-  #   end
-  # end
-  #
-  # def can_review?
-  #   reviewer? || moderator? || administrator?
-  # end
 end
